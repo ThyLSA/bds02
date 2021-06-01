af2601e4ff4f3d25084b1bda91f3b14bfa13e212
Thierry Almeida <thierryluiz@hotmail.com>
2021-06-01 20:44:45 -0300

    EventControllerIT passing

--- src/main/java/com/devsuperior/bds02/controllers/EventController.java
+++ src/main/java/com/devsuperior/bds02/controllers/EventController.java
@@ -1,9 +1,15 @@
 package com.devsuperior.bds02.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.bds02.dto.EventDTO;
 import com.devsuperior.bds02.services.EventService;
 
 @RestController
@@ -12,4 +18,10 @@ public class EventController {
 	
 	@Autowired
 	EventService service;
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<EventDTO> update(@PathVariable Long id, @RequestBody EventDTO dto){
+		dto = service.update(dto, id);
+		return ResponseEntity.ok().body(dto);
+	}
 }
---
+++ src/main/java/com/devsuperior/bds02/controllers/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.devsuperior.bds02.controllers.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.bds02.services.exceptions.DatabaseIntegrityException;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		StandardError error = new StandardError();
+		error.setTimestamp(Instant.now());
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setError("Resource not found");
+		error.setMessage(e.getMessage());
+		error.setPath(request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
+	}
+	
+	@ExceptionHandler(DatabaseIntegrityException.class)
+	public ResponseEntity<StandardError> databaseIntegrityException(DatabaseIntegrityException e, HttpServletRequest request){
+		StandardError error = new StandardError();
+		error.setTimestamp(Instant.now());
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setError("Database integrity violated");
+		error.setMessage(e.getMessage());
+		error.setPath(request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(error);
+	}
+}
